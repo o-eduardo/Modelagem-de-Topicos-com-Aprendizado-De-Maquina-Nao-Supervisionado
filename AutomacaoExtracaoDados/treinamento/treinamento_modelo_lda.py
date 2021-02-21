cefabc3c3a9bf242f3c6993385f6c7797c8bf9fe
o-eduardo <ca,eduardo94@gmail.com>
2021-02-20 21:24:45 -0300

    Ajustes treinamento modelo LDA.

--- AutomacaoExtracaoDados/treinamento/treinamento_modelo_lda.py
+++ AutomacaoExtracaoDados/treinamento/treinamento_modelo_lda.py
@@ -8,16 +8,17 @@ import gensim
 
 class TreinamentoModelo:
 
-    def obter_modelo_lda(self, corpus_artigos, dicionario_id2word, numero_de_topicos, hiper_param_alfa):
+    @staticmethod
+    def obter_modelo_lda(corpus_artigos, dicionario_artigo, numero_de_topicos, hiper_param_alfa):
         inicio = time.time()
 
         lda_model = gensim.models.LdaMulticore(corpus=corpus_artigos,
-                                               id2word=dicionario_id2word,
+                                               id2word=dicionario_artigo,
                                                num_topics=numero_de_topicos,
                                                random_state=100,
                                                # update_every=1,
                                                chunksize=100,
-                                               passes=10,
+                                               passes=50,
                                                alpha=hiper_param_alfa,
                                                eta=0.1,
                                                per_word_topics=True)
@@ -25,19 +26,25 @@ class TreinamentoModelo:
         tempo_execucao = round((fim - inicio) / 60)
         if tempo_execucao < 1:
             tempo_execucao = 1
-        print("Execução estimada da obtenção de um objeto de modelo LDA: Até {0} Min(s)".format(tempo_execucao))
+        #print("Execução estimada da obtenção de um objeto de modelo LDA: Até {0} Min(s)".format(tempo_execucao))
+
         return lda_model
 
-    def calcular_coerencia_modelo(self, modelo_lda, lista_de_lista_artigos, dicionario_id2word):
+    def calcular_coerencia_modelo(self, modelo_lda, lista_de_lista_artigos, dicionario):
+        #print("Iniciando o Calculo da coerencia de um objeto de modelo LDA")
+
         modelo_coerencia_lda = CoherenceModel(model=modelo_lda, texts=lista_de_lista_artigos,
-                                              dictionary=dicionario_id2word, coherence='c_v')
+                                              dictionary=dicionario, coherence='c_v')
         valor_coerencia_lda = modelo_coerencia_lda.get_coherence()
-        return round(valor_coerencia_lda, 5)
+        #print("Coerencia calculada com sucesso!\n")
+        print("\nCoerencia C_v: {0}".format(valor_coerencia_lda))
+
+        return round(valor_coerencia_lda, 4)
 
     def gerar_planilha_treinamento_modelo(self, corpus_de_dados, dicionario, lista_unificada, nome_arquivo):
         grid = {'Validation_Set': {}}
 
-        topics_range = [5, 15, 25, 50, 75]
+        topics_range = [5, 15, 25, 35, 45, 50, 55, 65, 75]
         # Alpha parameter
         # {0.01, 0.31, 0.61, 0909, symmetric, asymmetric}
 
@@ -47,12 +54,12 @@ class TreinamentoModelo:
 
         # Validation sets
         num_of_docs = len(corpus_de_dados)
-        corpus_sets = [  # gensim.utils.ClippedCorpus(corpus, num_of_docs*0.25),
-            # gensim.utils.ClippedCorpus(corpus, num_of_docs*0.5),
-            # gensim.utils.ClippedCorpus(corpus_artigos, num_of_docs*0.75),
-            corpus_de_dados]
+        corpus_sets = [#gensim.utils.ClippedCorpus(corpus_de_dados, num_of_docs*0.25),
+                       #gensim.utils.ClippedCorpus(corpus_de_dados, num_of_docs*0.5),
+                       #gensim.utils.ClippedCorpus(corpus_de_dados, num_of_docs*0.75),
+                       corpus_de_dados]
 
-        # corpus_title = ['75% Corpus', '100% Corpus']
+        #corpus_title = ['75% Corpus', '100% Corpus']
         corpus_title = ['100% Corpus']
 
         model_results = {'Conjunto_De_Validacao': [],
@@ -64,7 +71,7 @@ class TreinamentoModelo:
 
         barra_load = len(topics_range) * len(alpha) * len(corpus_sets) * len(beta)
 
-        print("\n\nPREVISAO DE TERMINO: {} MIN\n\n".format((barra_load * 1) / 60))
+        print("\n\nPREVISAO DE TERMINO: {} MIN\n\n".format((barra_load * 1)))
         if 1 == 1:
             pbar = tqdm.tqdm(total=barra_load)
 
@@ -77,23 +84,17 @@ class TreinamentoModelo:
                         # iterare through beta values
                         for b in beta:
                             # get the coherence score for the given parameters
-                            meu_modelo = self.obter_modelo_lda(corpus_artigos=corpus_sets[i],
-                                                               dicionario_id2word_artigos=dicionario,
-                                                               k_topicos=k,
-                                                               hiper_param_alfa=a)
+                            meu_modelo = TreinamentoModelo.obter_modelo_lda(corpus_sets[i], dicionario, k, a)
 
-                            cv = self.calcular_coerencia_modelo(modelo_lda=meu_modelo,
-                                                                lista_de_lista_artigos=lista_unificada,
-                                                                dicionario_id2word=dicionario)
-
+                            cv = self.calcular_coerencia_modelo(meu_modelo, lista_unificada, dicionario)
                             # Save the model results
+                            #print(" {}".format(cv))
                             pbar.update(1)
-                            print(cv)
                             model_results['Conjunto_De_Validacao'].append(corpus_title[i])
                             model_results['Topicos'].append(str(k))
                             model_results['Alpha'].append(str(a))
                             model_results['Beta'].append(str(b))
-                            model_results['Coerencia_Cv'].append(float(cv))
+                            model_results['Coerencia_Cv'].append(cv)
 
                             # pbar.update(1)
             pandas.DataFrame(model_results).to_csv(nome_arquivo + '.csv', index=False)
